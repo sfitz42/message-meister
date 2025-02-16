e4a2647eb8c65e6ef06cf86e6147dc4d32ecc100
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-02-16 02:47:21 +0000

    Add service bus components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
+import React from 'react'
 import './App.css'
 import { Footer } from './components/Footer/Footer'
 import { AwsMessageSender } from './forms/aws/Aws'
+import { ServiceBusMessageSender } from './forms/aws/service-bus/ServiceBus'
+
+enum MessageBroker {
+  AWS = "AWS SQS/SNS",
+  ServiceBus = "Azure Service Bus",
+}
 
 export function App() {
+  const [broker, setBroker] = React.useState(localStorage.getItem('provider') as MessageBroker || MessageBroker.AWS)
+
+  const handleProviderChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBroker(event.target.value as MessageBroker)
+
+    localStorage.setItem('provider', event.target.value)
+  }
+
   return (
     <>
       <h1>MessageMeister 📢📩📬</h1>
+
       <div className="card">
-        <select>
-          <option value="AWS">AWS SQS/SNS</option>
+        <select value={broker} onChange={handleProviderChange}>
+          <option value={MessageBroker.AWS}>{MessageBroker.AWS}</option>
+          <option value={MessageBroker.ServiceBus}>{MessageBroker.ServiceBus}</option>
         </select>
 
-        <AwsMessageSender />
+        {broker === MessageBroker.AWS && <AwsMessageSender />}
+        {broker === MessageBroker.ServiceBus && <ServiceBusMessageSender />}
       </div>
+
       <Footer />
     </>
   )
